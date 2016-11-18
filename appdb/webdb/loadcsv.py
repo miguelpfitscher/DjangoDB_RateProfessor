ec9903e7d5030aed7616c61af07f6d53bb31ac46
Miguel Pfitscher <miguel@packer-debian-8-amd64.digitalocean.com>
2016-11-18 14:51:00 -0500

    universidades populadas & loadcsv funcional

--- appdb/webdb/loadcsv.py
+++ appdb/webdb/loadcsv.py
@@ -4,21 +4,33 @@ csv_filepathname="/home/miguel/django/appdb/webdb/uni.csv"
 your_djangoproject_home="/home/miguel/django/appdb"
 import django 
 import sys,os
+
+
+
 sys.path.append(your_djangoproject_home)
 os.environ['DJANGO_SETTINGS_MODULE'] = 'appdb.settings'
 django.setup()
+
+
 from webdb.models import University
 
 import csv
-dataReader = csv.reader(open(csv_filepathname), delimiter=';', quotechar='"')
+
+#with open('some.csv', newline='', encoding='utf-8') as f:
+#        reader = csv.reader(f)
+#            for row in reader:
+#                        print(row)
+
+
+dataReader = csv.reader(open(csv_filepathname, encoding='utf-8'), delimiter=';', quotechar='"')
+
 
 for row in dataReader:
     if row[0] == '2011': # Ignore the header row, import everything else
         uni = University()
-        #uni.name = row[3]
-        #uni.sigla = row[4]
-        print (row[3])
-        #uni.statecode = row[2]
-        #uni.statename = row[3]
+        uni.name = row[2].encode('utf-8')
+        uni.sigla = row[3].encode('utf-8')
+        #print (row[2].encode('utf-8'))
+        #print (row[3].encode('utf-8'))
         uni.save()
 
